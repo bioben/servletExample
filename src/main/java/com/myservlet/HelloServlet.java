4261e0be116aa9bac4de75f070f9ecd2647a8bed
bioben <benjaminahnemail@gmail.com>
2023-02-24 17:56:49 -0500

    minor details, cleaning code

--- src/main/java/com/myservlet/HelloServlet.java
+++ src/main/java/com/myservlet/HelloServlet.java
@@ -1,7 +1,6 @@
 package com.myservlet;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
